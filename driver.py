78047f8e0d62a57fad858e84286135b86e3f5ffa
Maral-Pn <pourdayan.ma.trd@gmail.com>
2024-08-09 23:54:02 +1000

    RANGE

--- driver.py
+++ driver.py
@@ -1,4 +1,13 @@
 
+import pyspark as ps
+
 if __name__ == "__main__":
     print("Hello World!")
+
+    spark = ps.sql.SparkSession.builder.appName("Test").master("local[*]").getOrCreate()
+
+    df = spark.range(1,1000)
+
+    df.show()
+
 
